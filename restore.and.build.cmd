7c8c8d1daeaa6c147f83946ec8dfb2906606f4f2
ouarzy <epecoul@gmail.com>
2016-06-15 12:59:01 +0200

    Align cmd and sh build script

--- restore.and.build.cmd
+++ restore.and.build.cmd
@@ -1,9 +1,13 @@
 @echo off
 
 if not exist .paket\paket.exe (
+    @echo "Downloading Paket"
     .paket\paket.bootstrapper.exe
 )
+
+@echo "Restoring dependencies"
 .paket\paket.exe restore
 
+@echo "Running web server"
 set encoding=utf-8
 packages\FAKE\tools\FAKE.exe build.local.fsx %*
--- restore.and.build.sh
+++ restore.and.build.sh
@@ -1,6 +1,12 @@
 echo off
 
-sh ./restore.dependencies.sh
+if [ ! -f ".paket/paket.exe" ]; then
+  echo "Downloading Paket"
+  mono .paket/paket.bootstrapper.exe
+fi
+
+echo "Restoring dependencies"
+mono .paket/paket.exe restore
 
 echo "Running web server"
 mono packages/FAKE/tools/FAKE.exe run --fsiargs build.local.fsx
