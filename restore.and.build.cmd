b92554d3cec82524afcfdfc947043998cccca949
fpellet <florent.pellet@gmail.com>
2016-06-25 16:10:11 +0200

    Update message of build script (windows)

--- restore.and.build.cmd
+++ restore.and.build.cmd
@@ -8,6 +8,6 @@ if not exist .paket\paket.exe (
 @echo "Restoring dependencies"
 .paket\paket.exe restore
 
-@echo "Running web server"
+@echo "Build server"
 set encoding=utf-8
 packages\FAKE\tools\FAKE.exe build.local.fsx %*
