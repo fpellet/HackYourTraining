70c831db87eb99ff6389c7067c687c81678740e5
ouarzy <epecoul@gmail.com>
2016-06-27 16:47:54 +0200

    nb Interested Trainees is now a property in the angular controller

--- www/app.js
+++ www/app.js
@@ -5,12 +5,13 @@
     app.controller("hackYourTrainingController", function($scope, $http) {
         var self = this;
 
-        self.text = "";
         self.interestedTrainees = [];
+        self.nbInterestedTrainees = 0;
 
         var load = function() {
             $http.get('/interestedTrainees').success(function(data) {
                 self.interestedTrainees = data.interestedTrainees;
+                self.nbInterestedTrainees = self.interestedTrainees.length; 
             });
         };
 
